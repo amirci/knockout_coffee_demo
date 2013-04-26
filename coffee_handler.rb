ac977725f0d0dd0a6a7009f993503acb3f733f4e
Amir Barylko <amir@barylko.com>
2013-04-26 02:33:56 -0400

    Added view model for twitter demo

--- coffee_handler.rb
+++ coffee_handler.rb
@@ -3,9 +3,9 @@ require 'sinatra/base'
 class CoffeeHandler < Sinatra::Base
     set :views, File.dirname(__FILE__) + '/templates/coffee'
     
-    get "/js/*.js" do
-        filename = params[:splat].first
-        coffee filename.to_sym
+    get "/js/*.js" do      
+      filename = params[:splat].first
+      coffee filename.to_sym
     end
 end
 
